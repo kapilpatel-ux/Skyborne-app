634d5576a06f40a2ab56750ded2aa8475eb5399e
kapilpatel-ux <kapil.patel@svdigital.ae>
2026-01-07 19:06:40 +0530

    Add explore screen and assets

---
+++ src/assets/images/explore/index.ts
@@ -0,0 +1,10 @@
+export const ExploreImages = {
+  meditation: require('./meditation.png'),
+  trending1: require('./trending1.png'),
+  arrow: require('./arrow.png'),
+  yoga1: require('./yoga1.png'),
+  yoga2: require('./yoga2.png'),
+  yoga3: require('./yoga3.png'),
+  yoga4: require('./yoga4.png'),
+  yoga5: require('./yoga5.png'),
+};
